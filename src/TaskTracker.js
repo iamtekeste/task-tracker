7961488400f83c0926a5f1735165872a314b73b2
Tekeste G <tekeste.kidanu@gmail.com>
2016-10-18 16:20:48 -0400

    add simple media queries

--- src/TaskTracker.js
+++ src/TaskTracker.js
@@ -1,6 +1,6 @@
 var TaskTracker = {
     tasks: [
-        {"name":	"Test	Task	#1",	"date":	"12/01/2012",	"assigned":	"John	Doe"	},
+        {"name":	"TestTaskTestTaskTestTaskTestTaskTestTaskTestTask#1",	"date":	"12/01/2012",	"assigned":	"John	Doe"	},
         {"name":	"Test	Task	#2",	"date":	"12/02/2012",	"assigned":	"John	Doe"	},
         {"name":	"Test	Task	#3",	"date":	"12/03/2012",	"assigned":	"John	Doe"	},
         {"name":	"Test	Task	#4",	"date":	"12/04/2012",	"assigned":	"John	Doe"	},
